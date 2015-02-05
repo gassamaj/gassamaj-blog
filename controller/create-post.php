1cdd0077274722286f11c9b5a12696fa2fb65ded
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-02-05 14:04:23 -0800

    Video 23

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,9 @@
 <?php
+    require_once();
+    
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     
     echo "<p>Title: $title</p>";
     echo "<p>Post: $post</p>";
-    
+   
