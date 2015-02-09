88fa360f34d04d1ba351bdf1484dd6cd0113810a
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-02-09 14:20:42 -0800

    Video 28

--- model/Database.php
+++ model/Database.php
@@ -12,5 +12,13 @@ class Database {
        $this->password = $password;
        $this->database = $database;
        }
-}
+
+       public function openConnection() {
+           
+       }
+    
+       public function closeConnection () {
+           
+       }
+     }
 
