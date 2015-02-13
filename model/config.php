92c71325372c527aec12d3cd52bc0a9bc4be8d5d
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-02-13 14:37:24 -0800

    Video 35

--- model/config.php
+++ model/config.php
@@ -1,11 +1,16 @@
 <?php
     require_once(__DIR__ . "/database.php");
+    session_start();
     
-$path = "/gassamaj-blog/";
+    $path = "/gassamaj-blog/";
      
      $host = "localhost";
      $username = "root";
      $password = "root";
      $database = "blog_db";
      
+     if(!isset ($_SESSION["connection"])) {
      $connection =  new Database($host, $username, $password, $database);
+     $_SESSION["connection"] = $connection;
+     }
+     
