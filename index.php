016b7d87c1500e0dde30e9d83b736d87ee8e6c4d
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-02-26 14:05:38 -0800

    Did comits and added color

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<?php
-   require_once(__DIR__ . "/controller/login-verify.php");
-   require_once(__DIR__ . "/view/header.php");
-   if(authenticateUser ()) {
+ 
+<link type="text/css" rel="stylesheet" href="background.css">
+  
+    <?php
+    // It helps my code.
+   
+    require_once(__DIR__ . "/controller/login-verify.php");
+   // Verifies the login
+    require_once(__DIR__ . "/view/header.php");
+    if(authenticateUser ()) {
    require_once(__DIR__ . "/view/navigation.php");
    }
+   // So I can be able to post.
    require_once(__DIR__ . "/controller/create-db.php");
+   // I dont know
    require_once (__DIR__ . "/view/footer.php");
-   require_once(__DIR__ . "/controller/read-posts.php")
-   ?>
-
-        
+   // I can read all my posts.
+   require_once(__DIR__ . "/controller/read-posts.php"); 
+           ?>
+      
